622fbb73974643c5a09d42ebe5115300ad8a0633
John Micheal Acera <john.acera@sportality.se>
2025-04-17 19:10:01 +0800

    remove previous rendering changes

--- src/app/api/stream/route.ts
+++ src/app/api/stream/route.ts
@@ -1,12 +1,5 @@
 export async function GET() {
   try {
-    // Wake up the streaming service first
-  const res = await fetch(`${process.env.LIVE_STREAMING_SERVICE_URL}`);
-    // Slight delay to give Render time to wake it up
-    // await new Promise((resolve) => setTimeout(resolve, 20000));
-
-    // Then trigger your existing flow
-  if (res?.status === 200) {
     const res = await fetch(`${process.env.LIVE_STREAMING_SERVICE_URL}/stream/start`, {
       method: 'GET',
     });
@@ -18,13 +11,11 @@ export async function GET() {
     return new Response(JSON.stringify({ message: 'Stream started' }), {
       status: 200,
     });
-  }
   } catch (error) {
     return new Response(JSON.stringify({ error: 'Unable to start stream' }), {
       status: 500,
     });
   }
-  
 }
 
 export async function POST() {
