00f7bbbc3e6cc670c2e42c928af20c63037f0046
John Micheal Acera <john.acera@sportality.se>
2025-04-17 19:20:48 +0800

    new modification to test waking up the service again

--- src/app/api/health-check/route.ts
+++ src/app/api/health-check/route.ts
@@ -2,16 +2,23 @@ import { NextRequest } from 'next/server';
 
 export const dynamic = 'force-dynamic'; // this disables edge rendering
 
-export async function GET(req: NextRequest) {
-  const response = await fetch(`${process.env.LIVE_STREAMING_SERVICE_URL}/health`, {
-    method: 'GET',
-  });
-
-  if (!response.body) {
-    return new Response('No body in response', { status: 500 });
+export async function GET() {
+    try {
+      const res = await fetch(`${process.env.LIVE_STREAMING_SERVICE_URL}/stream/health`, {
+        method: 'GET',
+      });
+      
+  
+      if (!res.ok) {
+        throw new Error('Failed to start stream');
+      }
+  
+      return new Response(JSON.stringify({ message: 'Service is healthy' }), {
+        status: 200,
+      });
+    } catch (error) {
+      return new Response(JSON.stringify({ error: 'Unable to start stream' }), {
+        status: 500,
+      });
+    }
   }
-
-  return new Response(response.body, {
-    status: 200,
-  });
-}
